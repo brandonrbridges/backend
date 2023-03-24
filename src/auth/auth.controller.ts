e5c1f31d8aba8e12b11b7e5b52747f4627ca3035
Brandon Bridges <brandonrbridges@outlook.com>
2023-03-24 23:05:43 +0000

    Refresh Token implementation

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,61 +1,31 @@
-import {
-  Body,
-  Controller,
-  HttpException,
-  Inject,
-  NotFoundException,
-  Post,
-  UnauthorizedException,
-} from '@nestjs/common';
-
-import { UsersService } from 'src/users/users.service';
-
-import * as bcrypt from 'bcrypt';
-import * as jwt from 'jsonwebtoken';
+import { Body, Controller, Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
 
 @Controller('auth')
 export class AuthController {
-  constructor(private usersService: UsersService) {}
+  constructor(private readonly authService: AuthService) {}
 
   @Post('login')
-  async login(@Body() body): Promise<any> {
-    const user = await this.usersService.findOne({ email: body.email });
-
-    if (!user) {
-      throw new NotFoundException('User not found');
-    }
-
-    const isPasswordValid = await bcrypt.compare(body.password, user.password);
-
-    if (!isPasswordValid) {
-      throw new UnauthorizedException('Invalid credentials');
-    }
-
-    const token = await jwt.sign({ id: user.id }, process.env.JWT_SECRET);
-
-    return {
-      user: user.toJSON(),
-      token,
-    };
+  async login(@Body() data): Promise<any> {
+    return await this.authService.login(data.email, data.password);
   }
 
   @Post('register')
-  async register(@Body() body): Promise<any> {
-    const user = await this.usersService.findOne({ email: body.email });
-    if (user) {
-      throw new HttpException('User already exists', 401);
-    }
+  async register(@Body() data): Promise<any> {
+    return await this.authService.register(data);
+  }
 
-    const createdUser = await this.usersService.insertOne({
-      first_name: body.first_name,
-      last_name: body.last_name,
-      email: body.email,
-      password: bcrypt.hashSync(body.password, 10),
-      roles: ['unverifed'],
-    });
+  @Post('refresh-token')
+  async refresh(@Body() data): Promise<any> {
+    const { token } = data;
+
+    const decoded = await this.authService.verifyRefreshToken(token);
+    const user = await this.authService.getUserById(decoded.id);
+    const accessToken = this.authService.generateAccessToken(user);
 
     return {
-      user: createdUser.toJSON(),
+      user: user.toJSON(),
+      accessToken,
     };
   }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,100 @@
-import { Injectable } from '@nestjs/common';
+// Nest
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+  UnauthorizedException,
+} from '@nestjs/common';
+
+// Services
+import { UsersService } from 'src/users/users.service';
+
+// Modules
+import * as bcrypt from 'bcrypt';
+import * as jwt from 'jsonwebtoken';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(private readonly usersService: UsersService) {}
+
+  async login(email: string, password: string): Promise<any> {
+    const user = await this.usersService.findOne({ email });
+
+    if (!user) {
+      throw new NotFoundException('User not found');
+    }
+
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+
+    if (!isPasswordValid) {
+      throw new UnauthorizedException('Invalid credentials');
+    }
+
+    const accessToken = this.generateAccessToken(user);
+    const refreshToken = this.generateRefreshToken(user);
+
+    return {
+      user: user.toJSON(),
+      accessToken,
+      refreshToken,
+    };
+  }
+
+  async register(data): Promise<any> {
+    const user = await this.usersService.findOne({ email: data.email });
+    if (user) {
+      throw new BadRequestException('User already exists');
+    }
+
+    const createdUser = await this.usersService.insertOne({
+      first_name: data.first_name,
+      last_name: data.last_name,
+      email: data.email,
+      password: bcrypt.hashSync(data.password, 10),
+      roles: ['waitlist'],
+    });
+
+    const accessToken = this.generateAccessToken(createdUser);
+    const refreshToken = this.generateRefreshToken(createdUser);
+
+    return {
+      user: createdUser.toJSON(),
+      accessToken,
+      refreshToken,
+    };
+  }
+
+  async verifyRefreshToken(refreshToken: string): Promise<any> {
+    try {
+      return jwt.verify(refreshToken, process.env.JWT_SECRET);
+    } catch (error) {
+      throw new UnauthorizedException('Invalid refresh token');
+    }
+  }
+
+  async getUserById(id: string): Promise<any> {
+    const user = await this.usersService.findById(id);
+
+    if (!user) {
+      throw new NotFoundException('User not found');
+    }
+
+    return user;
+  }
+
+  generateAccessToken(user: any): string {
+    const accessToken = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+      expiresIn: '15m', // set the expiration time for the access token
+    });
+
+    return accessToken;
+  }
+
+  generateRefreshToken(user: any): string {
+    const refreshToken = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+      expiresIn: '7d', // set the expiration time for the refresh token
+    });
+
+    return refreshToken;
+  }
+}
